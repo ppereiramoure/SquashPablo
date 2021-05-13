3c3561e6e31cdf3d0ff99f771830f1e571649522
braiskiskos <bmartinezparedes@danielcastelao.org>
2021-05-13 17:06:31 +0200

    SquasColavorador

--- src/company/Main.java
+++ src/company/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Main");
+
+        System.out.println("Apruebalo");
+        System.out.println("Por");
+        System.out.println("Fa");
     }
 }
